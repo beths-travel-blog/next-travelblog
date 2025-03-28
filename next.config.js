13250762f4dc27126e60636e54f66010f2fd9320
Beth Enderwick <bethenderwick@hotmail.co.uk>
2025-03-28 16:37:55 +0000

    chore: update to hygraph assets

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-    compiler: {
+  compiler: {
     // ssr and displayName are configured by default
     styledComponents: true,
   },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.graphassets.com', // Allow images from Hygraph CDN
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
