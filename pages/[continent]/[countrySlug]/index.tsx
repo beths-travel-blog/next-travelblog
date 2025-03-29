4de9c3b35c775c43087a17c00ae35adf359b1dae
Beth Enderwick <bethenderwick@hotmail.co.uk>
2025-03-29 20:16:28 +0000

    chore: refactor country info styling

--- pages/[continent]/[countrySlug]/index.tsx
+++ pages/[continent]/[countrySlug]/index.tsx
@@ -2,13 +2,12 @@ import React from "react";
 import styled from "styled-components";
 import { GraphQLClient, gql } from "graphql-request";
 
-import Carousel from "../../../src/components/Carousel/Carousel";
-import { CarouselItem } from "../../../src/components/Carousel/Carousel";
+import Carousel, { CarouselItem } from "../../../src/components/Carousel/Carousel";
 import BlogCardGrid from "../../../src/components/BlogCardGrid/BlogCardGrid";
 import Grid from "../../../src/components/Grid/Grid";
 import GridItem from "../../../src/components/Grid/GridItem";
 import SafeHtml from "../../../src/elements/SafeHtml";
-import spacing from "../../../src/utils/spacing"
+import spacing from "../../../src/utils/spacing";
 
 const graphcms = new GraphQLClient(
   "https://api-eu-west-2.hygraph.com/v2/cl8rmtxc5316701uk7n83321r/master"
@@ -112,14 +111,10 @@ export const CarouselGridItem = styled(GridItem)`
 `;
 
 const CountryPosts = ({ country }: any) => {
-  const date = new Date(country.datePublished);
-
-  const imageCarousel = country.images.map((image: any, i: number) => {
-    return (
-      <CarouselItem key={i}>
-        <img src={image.url} alt={`Image ${i}`} />
-      </CarouselItem>
-    );
+  const date = new Date(country.datePublished).toLocaleDateString("en-GB", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
   });
 
   return (
@@ -127,31 +122,34 @@ const CountryPosts = ({ country }: any) => {
       <Grid columns={12}>
         <GridItem colSpan={6} colStart={4}>
           <Heading>{country.title}</Heading>
-          <h4>
-                {date.toLocaleDateString("en-GB", {
-                  day: "numeric",
-                  month: "short",
-                  year: "numeric",
-                })}
-          </h4>
+          <h4>{date}</h4>
         </GridItem>
         <CarouselGridItem colSpan={12} colStart={1}>
-          <Carousel>{imageCarousel}</Carousel>
+          <Carousel>
+            {country.images.map((image: any, i: number) => (
+              <CarouselItem key={i}>
+                <img src={image.url} alt={`Image ${i}`} />
+              </CarouselItem>
+            ))}
+          </Carousel>
         </CarouselGridItem>
         <GridItem colSpan={6} colStart={4}>
           <SafeHtml content={country.generalInfo ? country.generalInfo.html : ''}/>
-          <ComponentHeading>Tips</ComponentHeading>
-          <SafeHtml content={country.tips ? country.tips.html : ''}/>
-          <ComponentHeading>Visa</ComponentHeading>
-          <SafeHtml content={country.visa ? country.visa.html : ''}/>
-          <ComponentHeading>When to Visit</ComponentHeading>
-          {country.timeOfYear ? country.timeOfYear : ''}
-          <ComponentHeading>Travelling Around</ComponentHeading>
-          <SafeHtml content={country.travel ? country.travel.html : ''}/> 
-          <ComponentHeading>Sim Cards</ComponentHeading>
-          {country.simCards ? country.simCards : ''}
-          <ComponentHeading>ATMs</ComponentHeading>
-          {country.atms ? country.atms : ''}
+          {[
+            { title: "Tips", content: country.tips?.html },
+            { title: "Visa", content: country.visa?.html },
+            { title: "When to Visit", content: country.timeOfYear },
+            { title: "Travelling Around", content: country.travel?.html },
+            { title: "Sim Cards", content: country.simCards },
+            { title: "ATMs", content: country.atms },
+          ].map(({ title, content }, index) =>
+            content ? (
+              <React.Fragment key={index}>
+                <ComponentHeading>{title}</ComponentHeading>
+                  <SafeHtml content={content} />
+              </React.Fragment>
+            ) : null
+          )}
         </GridItem>
       </Grid>
       <BlogCardGrid blogPosts={country.places} postGrid={true}/>
